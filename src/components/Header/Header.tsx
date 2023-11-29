5e1fb212f5964e4c410eedceb5ef60d5c1a71908
yudai-shimoma <cdcdasdll@icloud.com>
2023-11-29 20:04:32 +0900

    ADD:ヘッダーにタブを追加

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,14 @@ const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <h1>My Website</h1>
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem}>About</li>
+          <li className={styles.navItem}>Services</li>
+          <li className={styles.navItem}>Contact</li>
+        </ul>
+      </nav>
     </header>
   );
 };
