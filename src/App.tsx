143f2a37b58018804cd257132f614e11d5ad2f51
yudai-shimoma <cdcdasdll@icloud.com>
2023-11-29 17:31:45 +0900

    ADD:Bootstrap React（React-Bootstrap）の導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
